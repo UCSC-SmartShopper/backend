d23985917971c545917b19bef50172eb76b7ff12
BimsaraAnjanaJay <bimsarajayadewa@gmail.com>
2024-07-28 11:06:07 +0530

    Insert values to the order and cartItem tables

---
+++ sql scripts/cartItem.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    "CartItem" (
+        "supermarketitemId",
+        "quantity",
+        "consumerId"
+    )
+VALUES (1, 2, 1),
+    (2, 1, 1),
+    (3, 4, 2),
+    (4, 3, 2),
+    (5, 5, 3);
